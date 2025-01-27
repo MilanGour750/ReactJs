1380d4ef689c88f5df02de6bedc6c7846cb4feee
Milan Gour <milan.gour97521@gmail.com>
2025-01-27 13:31:50 +0530

    V2

--- 10_Todolist(context)/src/App.jsx
+++ 10_Todolist(context)/src/App.jsx
@@ -1,16 +1,32 @@
 import { useState } from 'react'
-
+import Form from './components/Form'
+import { ToDoProvider } from './context/TodoContext'
 
 function App() {
+  const [todos, SetTodos] = useState([])
+  const addTodo = (todo) => {
+    SetTodos((prev)=> [{id: Date.now(), ...todo}, ...prev])
+  }
 
+  const updateToDo = (id, todo) => {
+    SetTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteToDo = (id) => {
+    SetTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = () =>{
+    SetTodos((prev) => prev.map((prevtodo)=> prevtodo === id ? {...prevtodo, completed: !prevtodo.completed} : prevtodo))
+  }
 
   return (
-    <>
+    <ToDoProvider value={{todos, addTodo, updateToDo, deleteToDo, toggleComplete}}>
       <div className='bg-[#172842] min-h-screen py-8'>
         <div className='w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white'>
           <h1 className='text-2xl font-bold text-center mb-8 mt-2'>Manage Your TO Do's</h1>
           <div className='mb-4'>
-            {/* todoform */}
+            <Form/>
           </div>
           <div className='flex flex-wrap gap-y-3'>
             {/* Loop & To DO */}
@@ -18,8 +34,11 @@ function App() {
         </div>
       </div>
 
-    </>
+    </ToDoProvider>
   )
 }
 
+
 export default App
+
+
---
+++ 10_Todolist(context)/src/components/Items.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Items() {
+  return (
+    <>
+        <div className={`flex border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black ${}`}>
+            <input 
+                type="text" 
+                
+            />
+        </div>
+    </>
+  )
+}
+
+export default Items
