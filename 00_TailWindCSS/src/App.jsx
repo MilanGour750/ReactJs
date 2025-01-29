22d2b16742cce7a8781250f3562e077594c404e5
Milan Gour <milan.gour97521@gmail.com>
2025-01-29 21:45:26 +0530

    Tailwind CSS Added

---
+++ 00_TailWindCSS/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-amber-300 text-3xl text-black'>This Is My WebPage</h1>
+    </>
+  )
+}
+
+export default App
