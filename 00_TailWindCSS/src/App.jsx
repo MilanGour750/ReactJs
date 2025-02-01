cf509324f9f1e2f8b46c4d72a331552b7c97ab04
Milan Gour <milan.gour97521@gmail.com>
2025-02-01 21:06:28 +0530

    NavBar

--- 00_TailWindCSS/src/App.jsx
+++ 00_TailWindCSS/src/App.jsx
@@ -6,29 +6,19 @@ function App() {
 
   return (
     <>
-    <div className='grid place-content-center h-screen bg-slate-500'>
-      <h1 className=' text-white text-4xl m-6'>TEST</h1>
-
-      <div className=''>
-        <div className='p-6 max-w-sm mx-auto bg-white rounded-2xl shadow-lg flex items-center space-x-4'>
-          <div >
-            <img className='h-12 w-12 rounded-full' src="https://picsum.photos/200" alt="Not Available" />
-          </div>
-          <div>
-            <div className='text-2xl font-medium'>
-              Tailwind CSS
-              <p className='bg-slate-300 text-base'>By Milan Gour</p>
-            </div>
-          </div>
-        </div>
-          <button className=' flex flex-wrap items-center text-center text-base px-3 mt-2 bg-blue-400 text-black rounded-sm mx-auto hover:bg-lime-300 cursor-pointer'>Click!</button>
-      </div>
-      <div className='text-center my-4 text-gray-950'>
-        <p className='text-white md:text-amber-500 sm:text-green-500 '>This is a test paragraph.</p>
-      </div>
-      </div>
-
       
+        <nav className='w-full h-14 bg-indigo-200 flex justify-between px-4 items-center md:px-4'>
+          <div className='text-2xl font-bold text-indigo-800'>Milan Tent House</div>
+            <ul>
+              <li>Home</li>
+              <li>Career</li>
+              <li>About Us</li>
+            </ul>
+          <div>
+            Login/SignUp
+          </div>
+        </nav>
+
     </>
   )
 }
